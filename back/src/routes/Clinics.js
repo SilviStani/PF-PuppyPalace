a679129633a171458b97a3d887c9904461fc66ff
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-10 17:02:53 -0300

    Removed reserves routes

--- back/src/routes/Clinics.js
+++ back/src/routes/Clinics.js
@@ -1,13 +1,11 @@
 const { Router } = require ('express');
-const { getClinics, postClinics, getClinicsId, postReserve, getReserve, getReserveId } = require("../controllers/Clinics.js");
+const { getClinics, postClinics, getClinicsId } = require("../controllers/Clinics.js");
 
 const router = Router();
 
 router.get('/', getClinics);
 router.get('/:id', getClinicsId);
 router.post('/', postClinics );
-router.post('/reserve', postReserve);
-router.get('/reserve', getReserve);
-router.get('/reserve/:id', getReserveId);
+
 
 module.exports = router;
