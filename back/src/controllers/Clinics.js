e5fe846ad5aafc2aef5fbf316c3151e2a5a2b6f0
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-03-10 17:01:16 -0300

    removed reserve controllers

--- back/src/controllers/Clinics.js
+++ back/src/controllers/Clinics.js
@@ -20,15 +20,6 @@ const idSearch = async(id) => {
     }
 }
 
-const reserveId = async(id) => {
-    try {
-        const idReserve = await Reserve.findByPk(id)
-        return idReserve;
-    } catch (error) {
-        return res.status(404).send(error)
-    }
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////
 // Acá van las RUTAS
 
@@ -83,68 +74,8 @@ const postClinics = async (req, res) => {
         }
 }
 
-// ----------------- Reserves ----------------- \\
-
-const getReserve = async (req, res) => {
-    try {
-        const reserve = await Reserve.findAll();
-        return res.status(200).json(reserve)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-}
-
-const getReserveId = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const idDetails = await reserveId(id)
-        if(!idDetails) {
-            return res.status(404).send("No hay reservas con este id")
-        }
-        res.status(200).json(idDetails)
-    } catch (error) {
-        res.status(404).send(error)
-    }
-}
-
-const postReserve = async (req, res) => {
-    let {
-        ammount,
-        date,
-        hourly,
-        description,
-        city,
-        userId,
-        clinicId
-
-    } = req.body;
-  
-    try{
-        if( ammount, date, hourly, description, city ) {
-            let newReserve = await Reserve.create({
-                ammount,
-                date,
-                hourly,
-                description,
-                city,
-                userId,
-                clinicId
-            })
-            res.status(201).json(newReserve)
-        } else {
-            res.status(401).send({error: "Por favor complete todos los campos"})
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(400).send(error)
-    }
-  }
-  
   module.exports = {
   getClinics,
   postClinics,
-  getClinicsId,
-  postReserve,
-  getReserve,
-  getReserveId
+  getClinicsId
   }
